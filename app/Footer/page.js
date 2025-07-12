00f79e8b1e039756b51840d5def57b06b09807b8
Prabh-Singh-1 <workprabhsingh1.com>
2025-07-12 14:53:47 +0530

    modified:   app/Footer/page.js

--- app/Footer/page.js
+++ app/Footer/page.js
@@ -16,26 +16,26 @@ const Footer = () => {
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">PRODUCT CATEGORIES</h2>
               <nav className="list-none mb-10">
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                  <Link href={"/Sports-Shoe"} className="text-gray-600 hover:text-gray-800">Sports Shoes</Link>
                 </li>
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                  <Link href={"/Snickers"} className="text-gray-600 hover:text-gray-800">Sneakers</Link>
                 </li>
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+                  <Link href={"/Loafers"} className="text-gray-600 hover:text-gray-800">Loafers</Link>
                 </li>
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                  <Link href={"/Boots"} className="text-gray-600 hover:text-gray-800">Formals and Boots</Link>
                 </li>
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
               <nav className="list-none mb-10">
-                <li>
+                <li>  
                   <a className="text-gray-600 hover:text-gray-800">First Link</a>
                 </li>
                 <li>
@@ -88,7 +88,7 @@ const Footer = () => {
         <div className="bg-gray-100">
           <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
             <p className="text-gray-500 text-sm text-center sm:text-left">© 2025 Urban Step —
-              <Link href={"#"} rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">Step up With Style</Link>
+              <Link href={"/"} rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">Step up With Style</Link>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
               <Link href={"#"} className="text-gray-500">
@@ -107,7 +107,7 @@ const Footer = () => {
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
               </Link>
-              <Link href={"#"} className="ml-3 text-gray-500">
+              <Link href={"https://www.linkedin.com/in/prabhleen-singh-244925281/"} className="ml-3 text-gray-500">
                 <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
                   <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                   <circle cx="4" cy="4" r="2" stroke="none"></circle>
